6c3c1339bdbdc09da12152082f962594724547b2
markoml <markoml@ipb.ac.rs>
2012-02-21 18:49:52 +0100

    first commit

---
+++ exercise.py
@@ -0,0 +1,24 @@
+import os
+import glob  
+       
+def  files(path):
+   	listing = os.listdir(path)
+        return listing
+def  filestring(line):
+     search=': N'
+     a=line.find(search)
+     if a>=0:
+	 b=True
+     else:
+	 b=False
+     return b;
+def  replace(line):
+     nline = line.replace(": N",": M")
+     return nline;		
+path='cleandata'
+filelist=files(path)
+for infile in filelist:
+	for inline in infile:
+		print filestring(inline)
+		print replace(inline) 
+        print "current file is: " + infile   
